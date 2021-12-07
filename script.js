868db61c8d873a24c0321bd2fbc3fe553db6f6bf
Justin Garner <j.garner828@gmail.com>
2021-12-07 18:16:45 -0500

    working on fetch token to spotify API

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 /*  https://developer.spotify.com/documentation/web-api/#response-schema
 *   https://developer.spotify.com/documentation/general/guides/authorization/client-credentials/
 *   https://developer.spotify.com/dashboard/applications
@@ -14,16 +10,20 @@ var client_secret = '45870bee58304f8d975f82ff44d06d0f';
 
 // to be base 64 encoded 'bf2b6b1e0d444912ba9ca99426b39f16:45870bee58304f8d975f82ff44d06d0f'
 
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
+
+fetch('https://accounts.spotify.com/api/token', {
   headers: {
-    'Authorization': 'Basic ' + 'YmYyYjZiMWUwZDQ0NDkxMmJhOWNhOTk0MjZiMzlmMTY6NDU4NzBiZWU1ODMwNGY4ZDk3NWY4MmZmNDRkMDZkMGY',
-    'Content-Type': 'application/x-www-form-urlencoded'
+    'Authorization': 'Basic ' + 'YmYyYjZiMWUwZDQ0NDkxMmJhOWNhOTk0MjZiMzlmMTY6NDU4NzBiZWU1ODMwNGY4ZDk3NWY4MmZmNDRkMDZkMGY'
   },
   form: {
     grant_type: 'client_credentials'
   },
   json: true
-};
-
-$.post(authOptions);
+})
+    .then(response => {
+      return response.json();
+  })
+    .then(data => {
+      console.log(data)
+    })
+  
