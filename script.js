aa9c766a6cc52c9981065d7db1708727d663bc3d
aalanz51 <austinalexanderlanz@gmail.com>
2021-12-11 12:48:12 -0500

    style adjustments / drag and drop added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //NAVBAR//
+var artistinput = document.getElementById("artistinput")
+
 jQuery(document).ready(function (e) {
   function t(t) {
       e(t).bind("click", function (t) {
@@ -24,17 +26,56 @@ jQuery(document).ready(function (e) {
   })
 });
 
-//CALENDAR//
-var dates = document.querySelectorAll("section time.hidden");
-    var i = 1;
-    Array.prototype.forEach.call(dates, function(caldate) {
-        setTimeout(function(){ caldate.classList.remove("hidden") }, 250*i)
-        i++;
-})
+//FAVORITES//
+document.addEventListener('DOMContentLoaded', (event) => {
+    var dragSrcEl = null;
+    function handleDragStart(e) {
+      this.style.opacity = '0.4';
+      dragSrcEl = this;
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      e.dataTransfer.dropEffect = 'move';
+      return false;
+    }
+    function handleDragEnter(e) {
+      this.classList.add('over');
+    }
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+    function handleDrop(e) {
+      if (e.stopPropagation) {
+        e.stopPropagation(); // stops the browser from redirecting.
+      }
+      if (dragSrcEl != this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+      }
+      return false;
+    }
+    function handleDragEnd(e) {
+      this.style.opacity = '1';
+      items.forEach(function (item) {
+        item.classList.remove('over');
+      });
+    }
+    let items = document.querySelectorAll('.dragcontainer .box');
+    items.forEach(function(item) {
+      item.addEventListener('dragstart', handleDragStart, false);
+      item.addEventListener('dragenter', handleDragEnter, false);
+      item.addEventListener('dragover', handleDragOver, false);
+      item.addEventListener('dragleave', handleDragLeave, false);
+      item.addEventListener('drop', handleDrop, false);
+      item.addEventListener('dragend', handleDragEnd, false);
+    });
+  });
 
 
-
-var artistinput = document.getElementById("artistinput")
 
 
 //bandsintown api 
